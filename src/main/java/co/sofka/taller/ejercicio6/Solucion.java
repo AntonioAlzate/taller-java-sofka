07b78b7eeb15b5ba31d307050809dfb7042e9001
Antonio Alzate <antonioalzate7@gmail.com>
2021-11-16 16:57:13 -0500

    Solución del ejercicio 6.

---
+++ src/main/java/co/sofka/taller/ejercicio6/Solucion.java
@@ -0,0 +1,14 @@
+package co.sofka.taller.ejercicio6;
+
+public class Solucion {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++){
+            if(i % 2 == 0)
+                System.out.println(i + " Es par");
+            else
+                System.out.println(i + " Es impar");
+        }
+    }
+}
